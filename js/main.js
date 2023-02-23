9f814865de8eb4a68228565191789a6e24d09626
DavideAtzo <atzo9477@gmail.com>
2023-02-23 15:44:32 +0100

    aggiiunta commenti js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 'use strict';
 
-
+// ciclo che va a stampare in console da 1 a 100 
 for (let i= 1; i <= 100; i++){
+    // ciclo ogni 15 va inserire la parola FizzBuzz 
     if(i % 15 === 0){
         console.log('FizzBuzz');
         
     }
+    // ciclo ogni 5 va inserire la parola Buzz
     else if(i % 5 === 0){
         console.log('Buzz');
     }
+    // ciclo ogni 3 va inserire la parola Fizz
     else if(i % 3 === 0){
         console.log('Fizz');
     }
-
+    // altrimenti stampa gli altri numeri 
     else {
         console.log(i);
     }
