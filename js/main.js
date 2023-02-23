5d5c33130467067844f133e1bd1a574eafad0056
DavideAtzo <atzo9477@gmail.com>
2023-02-23 16:40:45 +0100

    aggiunti passaggi readme + bonus 1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,41 @@
 'use strict';
 
+// dichiaro variabile che andrà ad inserire l'elemento in html 
+const container = document.querySelector('.container')
+let stringa = '';
+
 // ciclo che va a stampare in console da 1 a 100 
 for (let i= 1; i <= 100; i++){
+    const cards = document.createElement('div');
+    container.append(cards);
     // ciclo ogni 15 va inserire la parola FizzBuzz 
     if(i % 15 === 0){
+        stringa ='FizzBuzz'
         console.log('FizzBuzz');
-        
+        cards.append(stringa);
+        cards.classList.add('FizzBuzz');
     }
     // ciclo ogni 5 va inserire la parola Buzz
     else if(i % 5 === 0){
         console.log('Buzz');
+        stringa ='Buzz';
+        cards.append(stringa);
+        cards.classList.add('Buzz');
     }
     // ciclo ogni 3 va inserire la parola Fizz
     else if(i % 3 === 0){
         console.log('Fizz');
+        stringa ='Frizz';
+        cards.append(stringa);
+        cards.classList.add('Frizz');
+
     }
     // altrimenti stampa gli altri numeri 
     else {
         console.log(i);
+        cards.append(i);
     }
+
+
+
 }
--- readme.md
+++ readme.md
@@ -6,3 +6,12 @@ step-1:  creare ciclo che vada a stampare in console i numeri da 1 a 100;
 step-2: mettere la condizione che ad ogni multiplo di 3, stampa in console la parola "Fizz";
 step-3: mettere la condizione che ad ogni multiplo di 5, stampa in console la parola "Buzz";
 step-4: mettere la condizione che ad ogni multiplo di 3 e 5, stampa in console le parole dei precedenti step (Frizz e Buzz);
+
+BONUS 1:
+Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+
+Step-1: prendo container dall'html e dichiaro una variabile;
+Step-2: modifico il tipo di inserimento di frizz e buzz come valore di una variabile (stringa);
+Step-3: dichiara nuova variabile che ha valenza come aggiunta di un elemento (div), successivamente la inserirò dentro il container;
+Step-4: inserisco il valore di stringa dentro l'elemento creato;
+Step-5: inserisco il valore di quella stringa come classe per andarla poi a lavorare successivamente;
